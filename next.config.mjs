466b6adb28e9ba0790622397b6a4fdb329d61079
Owais Shaikh <alt@Owaiss-MacBook-Air.local>
2024-08-21 16:19:08 +0500

    Updated X-Frame-Options header to allow iframe embedding

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Adding headers to handle CORS for API routes
-//   async headers() {
-//     return [
-//       {
-//         // Matching all API routes
-//         source: '/api/:path*',
-//         headers: [
-//           {
-//             key: 'Access-Control-Allow-Credentials',
-//             value: 'true', // Allow credentials (cookies, authorization headers, etc.)
-//           },
-//           {
-//             key: 'Access-Control-Allow-Origin',
-//             value: 'https://altacademy.org', // Replace with your allowed origin
-//           },
-//           {
-//             key: 'Access-Control-Allow-Methods',
-//             value: 'GET,HEAD,POST,OPTIONS', // Allowed methods
-//           },
-//           {
-//             key: 'Access-Control-Allow-Headers',
-//             value: 'X-Requested-With, Accept, Content-Type, Authorization', // Allowed headers
-//           },
-//         ],
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -40,29 +8,29 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Credentials',
-            value: 'true',
+            value: 'true', // Allow credentials (cookies, authorization headers, etc.)
           },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://altacademy.org',
+            value: 'https://altacademy.org', // Replace with your allowed origin
           },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET,HEAD,POST,OPTIONS',
+            value: 'GET,HEAD,POST,OPTIONS', // Allowed methods
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Accept, Content-Type, Authorization',
+            value: 'X-Requested-With, Accept, Content-Type, Authorization', // Allowed headers
           },
         ],
       },
       {
-        // Matching all routes for iframe embedding headers
+        // Matching all routes for iframe embedding
         source: '/(.*)',
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'SAMEORIGIN', // Allows embedding on the same origin
+            value: 'https://altacademy.org',  // Allows embedding on the same origin
           },
           {
             key: 'Content-Security-Policy',
